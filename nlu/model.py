7681ee75325b4f8c511c90c16c00281438bab47e
Diego <diegosccpinto@gmail.com>
2023-03-25 17:39:40 -0300

    ch2byte

--- nlu/model.py
+++ nlu/model.py
@@ -13,37 +13,21 @@ for command in data['commands']:
     inputs.append(command['input'].lower())
     outputs.append('{}\{}'.format(command['entidade'], command['acao']))
 
-chars = set()
+max_seq_length = max([len(bytes(x.encode('utf-8'))) for x in inputs])
 
-for input_text in inputs + outputs:
-    for char in input_text:
-        if char not in chars:
-            chars.add(char)
-
-char2idx = {}
-idx2char = {}
-
-for i, char in enumerate(chars):
-    char2idx[char] = i
-    idx2char[i] = char
-
-max_seq_length = max([len(x) for x in inputs])
-
-print('Number of chars:', len(chars))
 print('Max sequence length:', max_seq_length)
 
-input_data_one_hot = np.zeros((len(inputs), max_seq_length, len(chars)), dtype='int32')
+input_data_one_hot = np.zeros((len(inputs), max_seq_length, 256), dtype='float32')
 for i, input_text in enumerate(inputs):
-    for k, char in enumerate(input_text):
-        input_data_one_hot[i, k, char2idx[char]] = 1.0
+    for k, char in enumerate(bytes(input_text.encode('utf-8'))):
+        input_data_one_hot[i, k, int(char)] = 1.0
 
-input_data_sparse = np.zeros((len(inputs), max_seq_length), dtype='int32')
+# input_data_sparse = np.zeros((len(inputs), max_seq_length), dtype='int32')
+#
+# for i, input_text in enumerate(inputs):
+#     for k, char in enumerate(input_text):
+#         input_data_sparse[i, k] = char2idx[char]
 
-for i, input_text in enumerate(inputs):
-    for k, char in enumerate(input_text):
-        input_data_sparse[i, k] = char2idx[char]
-
-# Output data
 
 labels = set(outputs)
 
@@ -64,10 +48,23 @@ output_data = to_categorical(output_data, len(output_data))
 print(output_data[0])
 
 model = Sequential()
-model.add(Embedding(len(chars), 64))
-model.add(LSTM(128, return_sequences=True))
+model.add(LSTM(128))
 model.add(Dense(len(output_data), activation='softmax'))
-
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+model.fit(input_data_one_hot, output_data, epochs=128)
 
-model.summary()
+
+def classificandoTexto(texto):
+    x = np.zeros((1, 48, 256), dtype='float32')
+
+    for k, ch in enumerate(bytes(text.encode('utf-8'))):
+        x[0, k, int(ch)] = 1.0
+
+    out = model.predict(x)
+    idx = out.argmax()
+    print(idx2label[idx])
+
+
+while True:
+    text = input('Digite algo: ')
+    classificandoTexto(text)
